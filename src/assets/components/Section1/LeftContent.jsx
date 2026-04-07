5838859103120ed1d57ffcec11eddbb1e2f5d0a7
Jatin Gupta <animatedworld434@gmail.com>
2026-04-07 15:57:55 +0530

    Add files via upload

---
+++ src/assets/components/Section1/LeftContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroText from './herotext'
+
+const LeftContent = () => {
+    return (
+        <div className='h-full flex flex-col justify-between w-1/3 '>
+            <HeroText />
+        </div>
+    )
+}
+
+export default LeftContent
